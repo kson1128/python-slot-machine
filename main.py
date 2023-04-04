db2249e9b2a52b8122ae4fff14c8db75dddc9731
kson1128 <69124193+kson1128@users.noreply.github.com>
2023-04-04 13:30:08 -0400

    add deposit func

---
+++ main.py
@@ -0,0 +1,15 @@
+MAX_LINES = 3
+
+
+def deposit():
+    while True:
+        amount = input("How much do you want to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0")
+        else:
+            print("Please enter a number")
+    return amount
